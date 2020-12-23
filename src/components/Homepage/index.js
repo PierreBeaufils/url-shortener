9dbb5684665689b9064dd3d4342a577edf3f28c9
Pierre Beaufils <pierre.beaufils@outlook.fr>
2020-12-23 15:17:58 +0100

    navbar

--- src/components/Homepage/index.js
+++ src/components/Homepage/index.js
@@ -1,9 +1,21 @@
 import './homepage.scss';
 
+import illustrationDesk from '../../images/illustration-working.svg';
+
 function Homepage() {
   return (
-    <header className="homepage">
-    </header>
+    <main className="homepage">
+      <div className="intro">
+        <div className="intro-text">
+          <h1>More than just shorter links</h1>
+          <p>Build your brand's recognition and get detailed insights on your links are performing</p>
+          <button className="button">Get Started</button>
+        </div>
+        <div className="intro-img">
+          <img src={illustrationDesk} alt="" />
+        </div>
+      </div>
+    </main>
   );
 }
 
